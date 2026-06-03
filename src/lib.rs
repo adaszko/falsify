6bba8c773f2bf72ec1581b4d233ab9990ea21825
Adam Szkoda <adaszko@gmail.com>
2026-06-03 12:12:46 +0200

    More semantically consistent API

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 
 use rand::{RngExt, rngs::StdRng};
 use std::cell::RefCell;
-use std::ops::Range;
 use std::ops::{Coroutine, CoroutineState};
 use std::pin::Pin;
 use std::rc::Rc;
@@ -11,26 +10,26 @@ use std::rc::Rc;
 pub trait ArbCoro<Y>: Coroutine<Yield = Y, Return = ()> {}
 impl<X, Y> ArbCoro<Y> for X where X: Coroutine<Yield = Y, Return = ()> {}
 
-pub fn arb_bool() -> impl ArbCoro<bool> {
+pub fn arb_bool(rng: Rc<RefCell<StdRng>>) -> impl ArbCoro<bool> {
     #[coroutine]
     move || {
-        yield false;
-        yield true;
+        loop {
+            let value: bool = {
+                let mut r = rng.borrow_mut();
+                r.random()
+            };
+            yield value;
+        }
     }
 }
 
-pub fn arb_range_usize(
-    range: Range<usize>,
-    rng: Rc<RefCell<StdRng>>,
-    num_tests: usize,
-) -> impl ArbCoro<usize> {
+pub fn arb_usize(rng: Rc<RefCell<StdRng>>) -> impl ArbCoro<usize> {
     #[coroutine]
     move || {
-        for _ in 0..num_tests {
-            let value = {
+        loop {
+            let value: usize = {
                 let mut r = rng.borrow_mut();
-                let v = r.random_range(range.clone());
-                v
+                r.random_range(usize::MIN..usize::MAX)
             };
             yield value;
         }
@@ -71,18 +70,28 @@ pub fn arb_triple<T>(mut arb_t: impl ArbCoro<T> + Unpin) -> impl ArbCoro<(T, T, 
     }
 }
 
-pub fn arb_vec<T>(mut arb_t: impl ArbCoro<T> + Unpin, len: usize) -> impl ArbCoro<Vec<T>> {
+pub fn arb_vec<T>(
+    mut arb_t: impl ArbCoro<T> + Unpin,
+    rng: Rc<RefCell<StdRng>>,
+    max_len: usize,
+) -> impl ArbCoro<Vec<T>> {
     #[coroutine]
     move || {
-        let mut v = Vec::with_capacity(len);
-        for _ in 0..len {
-            let t = match Pin::new(&mut arb_t).resume(()) {
-                CoroutineState::Yielded(t) => t,
-                CoroutineState::Complete(()) => return (),
+        loop {
+            let len = {
+                let mut r = rng.borrow_mut();
+                r.random_range(0..max_len)
             };
-            v.push(t);
+            let mut v = Vec::with_capacity(len);
+            for _ in 0..len {
+                let t = match Pin::new(&mut arb_t).resume(()) {
+                    CoroutineState::Yielded(t) => t,
+                    CoroutineState::Complete(()) => return (),
+                };
+                v.push(t);
+            }
+            yield v;
         }
-        yield v;
     }
 }
 
@@ -108,8 +117,12 @@ pub fn arb_rc<T>(mut arb_t: impl ArbCoro<T> + Unpin) -> impl ArbCoro<Rc<T>> {
     }
 }
 
-pub fn run<Y>(mut root_arb_coro: impl ArbCoro<Y> + Unpin, test: impl Fn(Y) -> bool) -> bool {
-    loop {
+pub fn run<Y>(
+    mut root_arb_coro: impl ArbCoro<Y> + Unpin,
+    num_tests: usize,
+    test: impl Fn(Y) -> bool,
+) -> bool {
+    for _ in 0..num_tests {
         match Pin::new(&mut root_arb_coro).resume(()) {
             CoroutineState::Yielded(val) => {
                 if !test(val) {
@@ -119,10 +132,7 @@ pub fn run<Y>(mut root_arb_coro: impl ArbCoro<Y> + Unpin, test: impl Fn(Y) -> bo
             CoroutineState::Complete(()) => return true,
         }
     }
-}
-
-pub trait Arb<Y> {
-    fn arb(&self, rng: Rc<RefCell<StdRng>>, size: usize) -> impl ArbCoro<Y>;
+    true
 }
 
 #[cfg(test)]
@@ -134,16 +144,16 @@ mod tests {
     #[test]
     fn frob() {
         let rng = Rc::new(RefCell::new(StdRng::try_from_rng(&mut SysRng).unwrap()));
-        let arb = arb_range_usize(0..100, rng, 100);
-        run(arb, |n| n % 2 == 0 || n % 2 == 1);
+        let arb = arb_usize(rng);
+        run(arb, 100, |n| n % 2 == 0 || n % 2 == 1);
     }
 
     #[test]
     fn frob_vec() {
         let rng = Rc::new(RefCell::new(StdRng::try_from_rng(&mut SysRng).unwrap()));
-        let arb_range_usize = arb_range_usize(0..100, rng, 100);
-        let arb_vec = arb_vec(arb_range_usize, 50);
-        run(arb_vec, |mut v| {
+        let arb_usize = arb_usize(rng.clone());
+        let arb_vec = arb_vec(arb_usize, rng, 50);
+        run(arb_vec, 100, |mut v| {
             let original = v.clone();
             v.reverse();
             v.reverse();
